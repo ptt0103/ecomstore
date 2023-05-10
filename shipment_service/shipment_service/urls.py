9f57d4b4c28d0c30ff75ed8343aa7e13588d62c0
ptt0103 <91577400+ptt0103@users.noreply.github.com>
2023-05-11 02:18:30 +0700

    Add files via upload

---
+++ shipment_service/shipment_service/urls.py
@@ -0,0 +1,24 @@
+"""shipment_service URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/4.1/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path
+from ship_status import views as ship_views
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('shipment_status/', ship_views.shipment_status),
+    path('shipment_updates/', ship_views.shipment_reg_update),
+]
